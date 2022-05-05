14cdc26b4ee65b67694e4223afb97ac3228f5d38
Musya <abashka1994@gmail.com>
2022-05-05 20:02:46 +0300

    add fonts

---
+++ src/js/modules/scroll.js
@@ -0,0 +1,18 @@
+const sections = [...document.getElementsByTagName('section')];
+let currentSection = 0;
+
+window.addEventListener('wheel', function(e) {
+  
+
+  
+});
+
+// function scrollToSection(i) {
+//     window.scrollTo({
+//         top: 1000,
+//         behavior: "smooth"
+//     });
+// //   document.getElementById(sections[i].id).scrollIntoView({
+// //     behavior: 'smooth'
+// //   });
+// }
