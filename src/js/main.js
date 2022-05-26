65acf20f99a78295e842e9f82ddb2decede3e640
Musya <abashka1994@gmail.com>
2022-05-26 20:52:44 +0300

    add

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   @@include('modules/svg.js');
   @@include('modules/polyfills.js');
+  @@include('modules/menu.js');
   @@include('modules/main-slider.js');
 });
