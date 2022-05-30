6a4932aecded3ae34c3ce8039ad95758de238d1e
Musya <abashka1994@gmail.com>
2022-05-30 21:38:51 +0300

    add

--- src/js/modules/main-slider.js
+++ src/js/modules/main-slider.js
@@ -1,22 +1,23 @@
-const swiper = new Swiper('.page-slider', {
-  loop: false,
-  slidesPerView: 1,
-  spaceBetween: 10,
-  mousewheel: {
-    invert: false,
-    releaseOnEdges: true,
-  },
-  forceToAxis: true,
-  eventsTarget: 'section',
-  breakpoints: {
-    768: {
-      slidesPerView: 1,
-      direction: 'vertical',
-    }
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-  },
-
-});
+if(document.querySelector('body').classList.contains('index')) {
+  const swiper = new Swiper('.page-slider', {
+    loop: false,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    mousewheel: {
+      invert: false,
+      releaseOnEdges: true,
+    },
+    forceToAxis: true,
+    eventsTarget: 'section',
+    breakpoints: {
+      768: {
+        slidesPerView: 1,
+        direction: 'vertical',
+      }
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+  });
+}
