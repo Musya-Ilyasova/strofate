3fba5692fd2fd033dd3affcbfcc3250f96ceadd9
Musya <abashka1994@gmail.com>
2022-06-15 22:48:34 +0300

    add natal page

--- src/js/modules/premium-form-list.js
+++ src/js/modules/premium-form-list.js
@@ -2,7 +2,6 @@ function premiumListToggle() {
   let premiumList = document.querySelector('.premium-list');
   let premiumListItems = premiumList.querySelectorAll('.premium-list-item');
   
-  premiumListHeight(premiumList, premiumListItems);
   premiumList.addEventListener('click', function(e){
     let target = e.target;
     if(target.classList.value == 'premium-list-item__header') {
@@ -13,19 +12,6 @@ function premiumListToggle() {
       target.parentNode.parentNode.classList.add('active');
     }
   })
-}
-
-function premiumListHeight(premiumList, premiumListItems) {
-  let sum;
-  premiumListItems.forEach((item) => {
-    let itemMargin =  Number(window.getComputedStyle(item, null).getPropertyValue("margin-bottom").replace("px",""));
-    let heightText =  Number(item.querySelector('p').scrollHeight); 
-    let heightHeader =   Number(item.querySelector('.premium-list-item__header').scrollHeight);
-
-    sum += heightHeader + heightText + (premiumListItems.length - 1) * 40 + itemMargin;
-    console.log(heightHeader + heightText + (premiumListItems.length - 1) * 40 + itemMargin);
-  })
-  // console.log(sum);
 }
 
 if(document.body.classList.contains('premium')) {
