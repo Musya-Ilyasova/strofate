eaedab4cee4e8dca3a3c45a14113927f8ab8c2f3
Musya <musya.ilyasova@getorca.app>
2022-07-13 01:07:04 +0300

    add

--- src/js/modules/compatibility-result.js
+++ src/js/modules/compatibility-result.js
@@ -10,17 +10,37 @@ function compatibilityResultLoad() {
     // вывести результат
     let results = JSON.parse(xhr.responseText);
     zodiacItemAdd(results);
+    resultsListAdd(results);
   }
 }
 
 function zodiacItemAdd(results) {
   let zodiacItem = document.querySelectorAll('.results-zodiac-item');
+  let i = 0;
   zodiacItem.forEach((item) => {
-    let itemIcon = document.createElement('svg');
-    itemIcon.classList.add('icon icon-gemini');
-    let itemIconUse= document.createElement('use');
+    let zodiacs = results[0]['zodiacs'][0];
+    let itemIcon = item.querySelector('svg');
+    itemIcon.classList = ('icon icon-'+ Object.keys(zodiacs)[i]);
+    let itemIconUse= item.querySelector('use');
+    itemIconUse.setAttribute('xlink:href', 'img/sprite/sprite.svg#'+ Object.keys(zodiacs)[i]);
+    let itemTitle = document.createElement('span');
+    itemTitle.classList.add('results-zodiac-item__title');
+    itemTitle.textContent = Object.keys(zodiacs)[i];
+    let itemSubtitle = document.createElement('span');
+    itemSubtitle.classList.add('results-zodiac-item__subtitle');
+    itemSubtitle.textContent = zodiacs[Object.keys(zodiacs)[i]];
+    item.append(itemTitle);
+    item.append(itemSubtitle);
+    i++;
   })
+}
 
-} 
+function resultsListAdd(results) {
+  let resultsList = results[0]['percentage-list'];
+  let list = document.querySelector('.results-list');
+  for(let i=0; i<=resultsList.length-1; i++) {
+
+  }
+}
 
 compatibilityResultLoad();
