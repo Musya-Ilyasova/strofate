17412ce9990c1e9e80f2dba4197670e484c4dee7
Musya <musya.ilyasova@getorca.app>
2022-07-21 05:08:25 +0300

    add

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -13,8 +13,24 @@ function burgerToggle() {
       body.style.overflow="hidden";
     }
   })
-  
+};
+
+function menuLocation() {
+  let links = document.querySelectorAll('.header-profil-menu-item__link');
+  links.forEach((link) => {
+    link.classList.remove('active');
+    let href = links.href;
+    let local = document.location.pathname.substring(1, document.location.pathname.length-5);
+    console.log(local);
+    if(local.indexOf(href)) {
+      link.classList.add('active');
+    };
+  })
+
 }
+
+menuLocation();
+
 
 if(document.querySelector('header').classList.contains('header-profil')) {
   burgerToggle();
